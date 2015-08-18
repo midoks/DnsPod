cd4e8c8f52b9b542c0d198471b3fd15a9f2a85b7
midoks <midoks@163.com>
2015-08-18 23:55:39 +0800

    重新修改关于我们

--- dnspod/UIDnsPod/MDnsPodTop/MAboutMeViewController.m
+++ dnspod/UIDnsPod/MDnsPodTop/MAboutMeViewController.m
@@ -8,7 +8,7 @@
 
 #import "MAboutMeViewController.h"
 
-@interface MAboutMeViewController ()
+@interface MAboutMeViewController () <UIWebViewDelegate>
 {
     UIWebView *_webView;
 }
@@ -25,11 +25,22 @@
     [self setTitleS:@"关于我们"];
     [self.view setBackgroundColor:[UIColor whiteColor]];
     
+    UIBarButtonItem  *rightButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(adviseMe)];
+    rightButton.tintColor = [UIColor whiteColor];
+    
+    self.navigationItem.rightBarButtonItem = rightButton;
     
     
-    NSString *html = @"<body style='background-color:#ccc;font-size:16px;'><p style='margin-top:50px;'>1.欢迎大家使用我和jason开发的DNSPod的iOS客服端。</p><p>2.本软件是根据DNSPod提供API开发完成。</p><p>3.本软件只对DnsPod上的域名进行管理。对域名进行添加、删除、暂停、启用。并对域名的记录,进行添加、删除、修改、暂停,启用。</p><p>4.本软件采用本地存储密码的方式,你可以使用注销的方式,清空所有信息。</p><p>5.本软件也对使用D令牌的用户进行了支持。</p><p>6.谢谢你的使用。对于我们的开发,有什么更好的意见。可以把你的意见,发送的我的邮件<a href='mailto:midoks@163.com'>midoks@163.com</a>(在使用期间,把你具体的需求描述出来,就更好了)。</p><hr /><p style='text-align:center;'>本软件是midoks和jason合作完成。</p></body>";
+    //NSString *html = @"<body style='background-color:#ccc;font-size:16px;'><p style='margin-top:50px;'>1.欢迎大家使用我和jason开发的DNSPod的iOS客服端。</p><p>2.本软件是根据DNSPod提供API开发完成。</p><p>3.本软件只对DnsPod上的域名进行管理。对域名进行添加、删除、暂停、启用。并对域名的记录,进行添加、删除、修改、暂停,启用。</p><p>4.本软件采用本地存储密码的方式,你可以使用注销的方式,清空所有信息。</p><p>5.本软件也对使用D令牌的用户进行了支持。</p><p>6.谢谢你的使用。对于我们的开发,有什么更好的意见。可以把你的意见,发送的我的邮件<a href='mailto:midoks@163.com'>midoks@163.com</a>(在使用期间,把你具体的需求描述出来,就更好了)。</p><hr /><p style='text-align:center;'>本软件是midoks和jason合作完成。</p></body>";
     _webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
-    [_webView loadHTMLString:html baseURL:nil];
+    _webView.delegate = self;
+    //[_webView loadHTMLString:html baseURL:nil];
+    
+    NSString *path      = [[NSBundle mainBundle] pathForResource:@"dnspod_about_me" ofType:@"html"];
+    NSURL *url          = [NSURL fileURLWithPath:path];
+    NSURLRequest *req   = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:req];
+    
     [self.view addSubview:_webView];
     
     MAboutMeViewControllerSingle = self;
@@ -52,9 +63,20 @@ static MAboutMeViewController *MAboutMeViewControllerSingle;
     _webView.frame = r;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - webViewDelegate -
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    //NSLog(@"ll");
+    [webView stringByEvaluatingJavaScriptFromString:@"rewrite();"];
+}
+
+
+#pragma mark 意见反馈
+-(void) adviseMe {
+    NSString *url = @"mailto:foo@example.com?cc=bar@example.com&subject=Greetings%20from%20Cupertino!&body=Wish%20you%20were%20here!";
+    [[UIApplication sharedApplication] openURL: [NSURL URLWithString: url]];
+    
+
 }
 
 @end
